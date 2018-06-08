da8fc89637203f0a437aae5de52bb81634e43c03
d-kiss <speakupness@gmail.com>
2018-06-08 14:45:33 +0300

    Hidden internal classes from end-user.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name="grep",
     packages=["grep"],
-    version="0.02",
+    version="0.03",
     description="String lookup has never been prettier.",
     author="Dan Elkis",
     author_email="elkissdan@gmail.com",
